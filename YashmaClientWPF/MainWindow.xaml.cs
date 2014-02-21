e3a0a86edecafabf0111aab30b2a9741c9c87d4a
Aswo <aswolo@yandex.ru>
2014-02-21 21:39:05 +0400

    Добавил деревовидное меню и вывод превьюшек.

--- YashmaClientWPF/MainWindow.xaml.cs
+++ YashmaClientWPF/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -10,6 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml;
 
 namespace YashmaClientWPF
 {
@@ -21,6 +23,75 @@ namespace YashmaClientWPF
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        public XmlDataProvider DataProvider { get; set; }
+
+        private void LoadTreeMenu()
+        {
+            DataProvider = new XmlDataProvider();
+            DataProvider.Source = new Uri(Environment.CurrentDirectory + "\\data\\tree_menu.xml");
+            DataProvider.XPath = "/NodeList";
+            this.tree_menu.DataContext = DataProvider;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            LoadTreeMenu();
+            Utility.LoadItems();
+        }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            //tree_menu.Width = Width / 100 * 25;
+           // content_scroll.Width = Width / 100 * 75;
+        }
+
+        private void TextBlock_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock textBlock = sender as TextBlock;
+            XmlAttribute xa = textBlock.Tag as XmlAttribute;
+            Update(Utility.SelectItems(xa.Value));
+        }
+
+        private void Update(List<TreeItem> tree_items)
+        {
+            content.Children.Clear();
+            content.ColumnDefinitions.Clear();
+            content.RowDefinitions.Clear();
+
+            int row_count = (int)Math.Ceiling(Width / 220) - 1;
+            int col_count = (int)Math.Ceiling((double)tree_items.Count / row_count);
+
+            for (int i = 0; i < row_count; i++) content.ColumnDefinitions.Add(new ColumnDefinition());
+
+            for (int i = 0; i < col_count; i++) content.RowDefinitions.Add(new RowDefinition());
+
+            for (int i = 0, m = 0; i < col_count; i++)
+            {
+                for (int j = 0; j < row_count; j++)
+                {
+                    if (m < tree_items.Count)
+                    {
+                        ImageSourceConverter imgs = new ImageSourceConverter();
+                        ContentElement contentElement = new ContentElement();
+                        contentElement.article.Content = tree_items[m].Name;
+                        contentElement.MouseUp += contentElement_MouseUp;
+                        contentElement.icon.SetValue(Image.SourceProperty, imgs.ConvertFromString(Environment.CurrentDirectory + "\\data\\images\\" + tree_items[m++].Image + "_p.png"));
+
+                        Grid.SetRow(contentElement, i);
+                        Grid.SetColumn(contentElement, j);
+
+                        content.Children.Add(contentElement);
+                    }
+                }
+            }
+            content.UpdateLayout();
+        }
+
+        void contentElement_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("1");
         }
     }
 }
--- YashmaClientWPF/Utility.cs
+++ YashmaClientWPF/Utility.cs
@@ -1,11 +1,62 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using System.Text;
+using System.Windows.Data;
+using System.Xml;
 
 namespace YashmaClientWPF
 {
     class Utility
     {
-        
+        public static List<TreeItem> tree_items = new List<TreeItem>();
+
+        public static List<TreeItem> SelectItems(string path)
+        {
+            List<TreeItem> new_tree_items = new List<TreeItem>();
+            foreach (TreeItem item in tree_items)
+            {
+                if (item.Tag.IndexOf(path) > -1)
+                {
+                    new_tree_items.Add(item);
+                }
+            }
+            return new_tree_items;
+        }
+
+        public static void LoadItems()
+        {
+            XmlTextReader reader = new XmlTextReader(Environment.CurrentDirectory + "\\data\\tree_menu.xml");
+            reader.WhitespaceHandling = WhitespaceHandling.None;
+
+            while (reader.Read())
+            {
+                if (reader.GetAttribute("Weight") != null)
+                {
+                    TreeItem treeItem = new TreeItem();
+                    treeItem.Name = reader.GetAttribute("Name");
+                    treeItem.Tag = reader.GetAttribute("Tag");
+                    treeItem.Weight = reader.GetAttribute("Weight");
+                    treeItem.Size = reader.GetAttribute("Size");
+                    treeItem.Сontent = reader.GetAttribute("Сontent");
+                    treeItem.Image = reader.GetAttribute("Image");
+                    treeItem.Description = reader.GetAttribute("Description");
+
+                    tree_items.Add(treeItem);
+                }
+            }
+            reader.Close();
+        }
+    }
+
+    class TreeItem
+    {
+        public string Name { get; set; }
+        public string Tag { get; set; }
+        public string Weight { get; set; }
+        public string Size { get; set; }
+        public string Сontent { get; set; }
+        public string Image { get; set; }
+        public string Description { get; set; }
     }
 }
